293978a5823f1de6f755fc3f4b6d65721eaccc33
MsEmily1020 <peace10200khs@gmail.com>
2024-01-23 12:59:55 +0900

    ✨ test db와 mysql 분리

---
+++ src/test/java/com/ost/matie/controller/user/UserControllerTest.java
@@ -0,0 +1,17 @@
+package com.ost.matie.controller.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+class UserControllerTest {
+
+    @Test
+    void addUser() {
+    }
+}
